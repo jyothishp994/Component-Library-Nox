9b27baf445443d92450ea667d0677947a58dacd7
decimalon <jyothishp994@gmail.com>
2022-03-10 20:37:08 +0530

    Feat: Add P1 Screens

--- js/apps.js
+++ js/apps.js
@@ -1,9 +1,16 @@
 submitvar=document.querySelector("#btnSubmit");
 username_err_var=document.querySelector(".username");
 password_err_var=document.querySelector(".password");
+modal_box_var=document.querySelector(".overlay-modal");
+toast_box_var=document.querySelector(".toast-box-content");
+modal_box_close_var=document.querySelector("#modal-close");
+console.log(modal_box_close_var)
+if(submitvar)
+{
+submitvar.addEventListener("click", validate);
+}
 
-submitvar.addEventListener("click", fn);
-function fn(){
+function validate(){
     usernamevar=document.querySelector("#username");
     passwordvar=document.querySelector("#password");
     if(!username.value){
@@ -12,4 +19,34 @@ function fn(){
     if(!password.value){
         password_err_var.innerText="Please enter the password"
             }
+}
+
+modalvar=document.querySelector("#modal-id");
+if(modalvar)
+{
+modalvar.addEventListener("click", ()=>
+{
+    modal_box_var.style.display="block";
+
+}
+);
+}
+if(modal_box_close_var)
+{
+    modal_box_close_var.addEventListener("click", ()=>
+    {
+        modal_box_var.style.display="none";
+    
+    }); 
+
+}
+
+toastvar=document.querySelector("#toast-id");
+if(toastvar)
+{
+    toastvar.addEventListener("click", ()=>
+{
+    toast_box_var.style.display="block";
+
+});
 }
